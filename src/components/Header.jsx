bfc476ceb971d1ba4e7fcc11458bc6bb9b3eb322
chome2xx <wataru.hirota@me.com>
2021-03-12 15:21:07 +0900

    20210312

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
-    return (
-        <div className='Header'>
-            <h1>Kanban</h1>
-            <hr/>
-        </div>
-    )
-}
+  return (
+    <div className="Header">
+      <div className="logo">
+        <h1>Kanban</h1>
+      </div>
+      <div className="filterArea">
+        <span className="label">Filter: </span>
+        <input type="text" className="filter" />
+      </div>
+      <div className="credential"></div>
+      <div className="clear"></div>
+      <hr />
+    </div>
+  );
+};
 
-export default Header
+export default Header;
